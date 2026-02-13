c15217726d2847825013c938be2ebb7150b886c7
nWillian1 <willian.nogueira2016@hotmail.com>
2026-02-13 15:49:00 -0300

    feat: registro de modelo Post no Django admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,12 @@ from django.contrib import admin
 # Register your models here.
 
 from .Models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_on', 'status') # Campos a serem exibidos na lista de posts
+    list_filter = ('status', 'created_on') # Filtros para status e data de criação
+    search_fields = ['title', 'content'] # Campos para pesquisa
+    prepopulated_fields = {'slug': ('title',)} # Gerar automaticamente o slug a partir do título
+
+
+admin.site.register(Post, PostAdmin) # Registrando o modelo Post no admin do Django com a configuração personalizada PostAdmin
